82a32cd25918a085cb92b179df6a631bc2d9673a
Piotr Zolnierek <pz@anixe.pl>
2012-05-02 11:46:54 +0200

    Add Validation Monad

--- lib/monadic/errors.rb
+++ lib/monadic/errors.rb
@@ -1,4 +1,7 @@
 module Monadic
   # thrown by Option#fetch if it has no value and no default was provided
   class NoValueError < StandardError; end
+
+  # thrown when a Success or Failure was expected, but anything else was returned
+  class NotEitherError < StandardError; end 
 end
---
+++ spec/validation_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'Validation' do
+  Person = Struct.new(:age, :gender, :sobriety, :name)
+
+  it 'a projection of Success and Failure' do
+
+    def validate(person)
+      check_age = ->(age_expr) {
+        age = age_expr.to_i
+        case 
+        when age <=  0; Failure('Age must be > 0')
+        when age > 130; Failure('Age must be < 130')
+        else Success(age)
+        end
+      }
+
+      check_sobriety = ->(sobriety) {
+        case sobriety
+        when :sober, :tipsy; Success(sobriety)
+        when :drunk        ; Failure('No drunks allowed')
+        else Failure("Sobriety state '#{sobriety}' is not allowed")
+        end 
+      }
+
+      Validation() do
+        check { check_age.(person.age);          }
+        check { check_sobriety.(person.sobriety) }
+      end
+    end
+
+    failure = validate(Person.new(age = 's', gender = :male, sobriety = :drunk, name = 'test'))
+    failure.should be_a Failure
+    failure.success?.should be_false
+    failure.failure?.should be_true
+    failure.should == Failure(["Age must be > 0", "No drunks allowed"])
+
+    success = validate(Person.new(age = 30, gender = :male, sobriety = :sober, name = 'test'))
+    success.should be_a Success
+    success.should == Success([])
+  end
+end
