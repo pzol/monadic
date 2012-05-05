39a45cee5b1c2f3141184cf24b9849e9a2b38b2d
Piotr Zolnierek <pz@anixe.pl>
2012-05-05 07:51:51 +0200

    Add #map spec to all Monads

--- spec/monad_axioms.rb
+++ spec/monad_axioms.rb
@@ -32,6 +32,26 @@ shared_examples 'a Monad' do
       end
 
       id1.should == id2
-    end    
+    end
+  end
+
+  describe '#map functor' do
+    add100 = ->(value) { value + 100 }
+    it 'on value types, returns the transformed value, wrapped in the Monad' do
+      res = monad.unit(1).map {|v| add100.(v) }
+      res.should == monad.unit(101)
+
+      res = monad.unit(1).map {|v| monad.unit(v + 100) }
+      res.should == monad.unit(101)
+    end
+
+    it 'on enumerables, returns the transformed collection, wrapped in the Monad' do
+      res = monad.unit([1,2,3]).map {|v| add100.(v) }
+      res.should == monad.unit([101,102,103])
+
+      # The following does not work... not sure whether it should
+      # res = monad.unit([1,2,3]).map {|v| monad.unit(v + 100) }
+      # res.should == monad.unit([101,102,103])
+    end
   end
 end
