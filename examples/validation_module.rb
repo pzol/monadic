b405778712b252031b10cfc596ef649f5e77a29f
Piotr Zolnierek <pz@anixe.pl>
2012-05-06 21:08:24 +0200

    Refactored example

--- examples/validation_module.rb
+++ examples/validation_module.rb
@@ -2,45 +2,45 @@ require 'monadic'
 
 Person = Struct.new(:name, :age)
 
-module BuildPerson
+module PersonValidation
   class AgeTooLowError < ArgumentError;  end
   class AgeTooHighError < ArgumentError; end
 
-  module Validators
-    def valid_age(expr)
-      age = expr.to_i
-      case 
-      when expr.nil?; Failure(ArgumentError.new(:age))
-      when age <=  0; Failure(AgeTooLowError.new(expr))
-      when age > 130; Failure(AgeTooHighError.new(expr))
-      else Success(age)
-      end
-    end
-
-    def valid_name(name)
-      case 
-      when name =~ /\w{3,99}/i; Success(name)
-      else Failure('Invalid name')
-      end
+  def self.validate(params)
+    Validation()  do
+      extend PersonValidation
+      check { valid_age(params[:age])  }
+      check { valid_name(params[:name])}
     end
   end
 
+  def valid_age(expr)
+    age = expr.to_i
+    case 
+    when expr.nil?; Failure(ArgumentError.new(:age))
+    when age <=  0; Failure(AgeTooLowError.new(expr))
+    when age > 130; Failure(AgeTooHighError.new(expr))
+    else Success(age)
+    end
+  end
+
+  def valid_name(name)
+    case 
+    when name =~ /\w{3,99}/i; Success(name)
+    else Failure('Invalid name')
+    end
+  end
+end
+
+module PersonBuild
   def self.build(params)
-    result = self.validate(params)
+    result = PersonValidation.validate(params)
     case result
     when Failure; result
     when Success; Person.new(*result.fetch)
     end
   end
-
-  def self.validate(params)
-    Validation()  do
-      extend BuildPerson::Validators
-      check { valid_age(params[:age])  }
-      check { valid_name(params[:name])}
-    end
-  end
 end
 
-p person1 = BuildPerson.build({ name: 'Andrzej' })            # Failure([#<ArgumentError: age>])
-p person2 = BuildPerson.build({ name: 'Andrzej', age: 32 })   # <struct Person name=32, age="Andrzej">
+p person1 = PersonBuild.build({ name: 'Andrzej' })            # Failure([#<ArgumentError: age>])
+p person2 = PersonBuild.build({ name: 'Andrzej', age: 32 })   # <struct Person name=32, age="Andrzej">
