77d8fc26f91b380cc5d5a3a8622ca2ae13c10cab
Piotr Zolnierek <pz@anixe.pl>
2012-06-17 12:25:38 +0200

    Refactor request example

--- spec/examples/hotel_booking_spec.rb
+++ spec/examples/hotel_booking_spec.rb
@@ -3,7 +3,7 @@ require 'active_support/time'
 
 describe 'Hotel Booking Example' do
 
-   module Request
+  class Request < Struct
     def self.create(params, params_module)
       value = self.create_value(params || {}, params_module)
 
@@ -14,13 +14,22 @@ describe 'Hotel Booking Example' do
       end
     end
 
+    def to_hash
+      Hash[*members.zip(values).flatten]
+    end
+
+    def unwrap
+      Struct.new(*members).new(*values.map(&:fetch))
+      # self.class.new(*values.map(&:fetch))
+    end
+
     private
     def self.create_value(params, params_module)
       properties = params_module.methods - Module.methods
-      request = Struct.new(*properties)
-      properties.reduce(request.new) do |container, property|
-        container[property] = params_module.send(property, params)
-        container
+      request_klas = self.new(*properties)
+      properties.reduce(request_klas.new) do |request, property|
+        request[property] = params_module.send(property, params)
+        request
       end
     end
   end
@@ -49,47 +58,76 @@ describe 'Hotel Booking Example' do
       end
     end
 
+    VALID_ROOM_TYPE = %w[SR DR TR QR]
     def room_type(params)
       case value = params['room_type']
       when nil
         Failure('room_type must not be empty')
-      else 
+      when is_valid_room_type
         Success(value)
+      else
+        Failure("room_type '#{value}' must be one of #{VALID_ROOM_TYPE.join(', ')}")
       end
+    end
+
+    def check_in(params)
+      param = params['check_in']
+      return Failure("check_in must not be empty")    if param.nil?
+      return Try { Date.parse(param) }.else {|e| "check_in #{e.message} '#{param}'" }
+    end
+
+    private
+    def is_valid_room_type
+      Object.new.tap {|matcher|
+        def matcher.===(value)
+          VALID_ROOM_TYPE.include? value
+        end
+      }
     end
   end
 
-  class Struct
-    def to_hash
-      Hash[*members.zip(values).flatten]
-    end
-
-    def unwrap
-      self.class.new(*values.map(&:fetch))
-    end
+  let(:params_proto) do
+    {'hotel_code'  => 'STO0001',
+     'check_in'    => '2012-06-15',
+     'nights'      => '3',
+     'room_type'   => 'DR',
+     'guest1'      => 'Max Payne',
+     'guest2'      => 'Jenny Payne'}
   end
 
   it 'builds a valid request' do
-    params = {'hotel_code' => 'STO0001',
-     'check_in'   => '2012-06-15',
-     'nights'     => '3',
-     'room_type'  => 'DR',
-     'guest1'     => 'Max Payne',
-     'guest2'     => 'Jenny Payne'}
-    request = Request.create(params, HotelBookingRequestParams)
-    request.should be_a Success
-    value = request.fetch.unwrap
-    value.hotel_code.should eq "STO0001"
-    value.nights.should eq 3
+    result = Request.create(params_proto, HotelBookingRequestParams)
+    result.should be_a Success
+    request = result.fetch.unwrap
+    request.hotel_code.should eq "STO0001"
+    request.nights.should eq 3
+  end
+
+  it 'reports an invalid room_type' do
+    params = params_proto.merge 'room_type' => 'XX'
+    result = Request.create(params, HotelBookingRequestParams)
+    result.should be_a Failure
+    request = result.fetch
+    request.room_type.should be_a Failure
+  end
+
+  it 'reports an invalid check_in time' do
+    params = params_proto.merge 'check_in' => '2012-11-31'
+    result = Request.create(params, HotelBookingRequestParams)
+    result.should be_a Failure
+    request = result.fetch
+    request.check_in.should be_a Failure
+    request.check_in.should == Failure("check_in invalid date '2012-11-31'")
   end
 
   it 'builds a failure request with no params' do
-    request = Request.create(nil, HotelBookingRequestParams)
-    request.should be_a Failure
-    value = request.fetch
-    value.hotel_code.should be_a Failure
-    value.nights.should be_a Failure
-    value.room_type.should be_a Failure
+    result = Request.create(nil, HotelBookingRequestParams)
+    result.should be_a Failure
+    request = result.fetch
+    request.hotel_code.should be_a Failure
+    request.nights.should be_a Failure
+    request.room_type.should be_a Failure
+    request.check_in.should be_a Failure
   end
 
 end
